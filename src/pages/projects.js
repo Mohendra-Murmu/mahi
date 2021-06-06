99ae50cd13b5d994747f923fe673d26969fd5aa4
Mahendramurmu <mahendradipu@gmail.com>
2021-06-06 11:55:24 +0530

    new

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -16,13 +16,23 @@ export default function Projects() {
                     <ExternalLink href="https://clone-fa48c.firebaseapp.com/">
                     <div className="work__img">                        
                              <img src="https://firebasestorage.googleapis.com/v0/b/instagram-clone016.appspot.com/o/images%2Famazon-clone.jpg?alt=media&token=91d12bf5-43a3-419b-91a8-1f6b68c06483"
-                         alt="" />                         
+                         alt="" />   
+                         <div className="work__title">Amazon React App</div>
                     </div>
                     </ExternalLink>
                     <ExternalLink href='https://instagram-clone016.firebaseapp.com'>
                     <div className="work__img">
                         <img src="https://firebasestorage.googleapis.com/v0/b/instagram-clone016.appspot.com/o/images%2Finstagram.png?alt=media&token=7cce4a49-f657-4d57-bbd1-02ea4e3f9ed9"
                          alt="" />
+                         <div className="work__title">Instagram React App</div>
+                    </div>                    
+                    </ExternalLink>
+                    <ExternalLink href='https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40darlin_g/weathernow-eda4267800e04877bfc01b375e08337a-signed.apk'>
+                    <div className="work__img">
+                        <img src="./weather-now.png"
+                         alt=""
+                         height="150px" />
+                         <div className="work__title">React Native Weather App</div>
                     </div>                    
                     </ExternalLink>
                 </div>
